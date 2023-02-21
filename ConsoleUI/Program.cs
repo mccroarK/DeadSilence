334556711695b3afc6851cd07cf1601b2fcd8137
Kevin McCroary <mccroark8827@student.faytechcc.edu>
2023-02-20 20:00:28 -0500

    All variables filled out

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -19,39 +19,39 @@ namespace ConsoleUI
         {
             // Local Variables
             // Rooms - 5
-            String room1 = "";
-            String room2 = "";
-            String room3 = "";
-            String room4 = "";
-            String room5 = "";
+            String room1 = "Lived-in Cabin";
+            String room2 = "Dim Cabin";
+            String room3 = "Bloody Cabin";
+            String room4 = "Cliffside Outpost";
+            String room5 = "Hoarder's Cabin";
 
             // Weapons - 4
-            String weapon1 = "";
-            String weapon2 = "";
-            String weapon3 = "";
-            String weapon4 = "";
+            String weapon1 = "Wood Chair";
+            String weapon2 = "Kitchen Knife";
+            String weapon3 = "Metal Pipe";
+            String weapon4 = "Pocket Revolver";
 
-            // Potions - 2
-            String food1 = "";
-            String food2 = "";
+            // Potions (Edible Items) - 2
+            String food1 = "Soft Drink [Cherry]";
+            String food2 = "Energy Drink";
 
-            // Treasure - 3
-            String treasure1 = "";
-            String treasure2 = "";
-            String treasure3 = "";
+            // Treasure (Key/Unique Items) - 3
+            String treasure1 = "$10 Bill";
+            String treasure2 = "Family Photo";
+            String treasure3 = "Ornate Watch";
 
             // Items - 4
-            String item1 = "";
-            String item2 = "";
-            String item3 = "";
-            String item4 = "";
+            String item1 = "Pebble";
+            String item2 = "Glass Bottle";
+            String item3 = "Cooking Pot";
+            String item4 = "Lighter";
 
             // Mobs - 5
-            String mob1 = "";
-            String mob2 = "";
-            String mob3 = "";
-            String mob4 = "";
-            String mob5 = "";
+            String mob1 = "Zid";
+            String mob2 = "Newcomer";
+            String mob3 = "The Screamer";
+            String mob4 = "Abomination";
+            String mob5 = "The Beast";
 
             Console.WriteLine("[1] Display Rooms");
             Console.WriteLine("[2] Display Weapons");
@@ -69,43 +69,60 @@ namespace ConsoleUI
                 case "1":
                 case "room":
                 case "rooms":
-                    // Display Rooms
-                    Console.WriteLine("Rooms"); // Placeholder
+                    // Display Rooms (5)
+                    Console.WriteLine(room1);
+                    Console.WriteLine(room2);
+                    Console.WriteLine(room3);
+                    Console.WriteLine(room4);
+                    Console.WriteLine(room5);
                     break;
 
                 case "2":
                 case "weapon":
                 case "weapons":
-                    // Display Weapons
-                    Console.WriteLine("Weapons"); // Placeholder
+                    // Display Weapons (4)
+                    Console.WriteLine(weapon1);
+                    Console.WriteLine(weapon2);
+                    Console.WriteLine(weapon3);
+                    Console.WriteLine(weapon4);
                     break;
 
                 case "3":
                 case "potion":
                 case "potions":
-                    // Display Potions
-                    Console.WriteLine("Potions"); // Placeholder
+                    // Display Potions (2)
+                    Console.WriteLine(food1);
+                    Console.WriteLine(food2);
                     break;
 
                 case "4":
                 case "treasure":
                 case "treasures":
-                    // Display Treasures
-                    Console.WriteLine("Treasures"); // Placeholder
+                    // Display Treasures (3)
+                    Console.WriteLine(treasure1);
+                    Console.WriteLine(treasure2);
+                    Console.WriteLine(treasure3);
                     break;
 
                 case "5":
                 case "item":
                 case "items":
-                    // Display Items
-                    Console.WriteLine("Items"); // Placeholder
+                    // Display Items (4)
+                    Console.WriteLine(item1); 
+                    Console.WriteLine(item2);
+                    Console.WriteLine(item3);
+                    Console.WriteLine(item4);
                     break;
 
                 case "6":
                 case "mob":
                 case "mobs":
-                    // Display Mobs
-                    Console.WriteLine("Mobs"); // Placeholder
+                    // Display Mobs (5)
+                    Console.WriteLine(mob1);
+                    Console.WriteLine(mob2);
+                    Console.WriteLine(mob3);
+                    Console.WriteLine(mob4);
+                    Console.WriteLine(mob5);
                     break;
 
                 case "7":
@@ -116,7 +133,7 @@ namespace ConsoleUI
 
                 default:
                     // If user enters an invalid answer
-                    Console.WriteLine("Answer out of range");
+                    Console.WriteLine("Answer is invalid");
                     break;
             }
 
